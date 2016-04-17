9abccf6961e2cde4e5143325630b353bb7101cb3
Ronen Lahat <ronenmail@gmail.com>
2016-04-17 08:20:33 +0300

    Trying to implement the ActionBar and Tabs from http://www.cs.dartmouth.edu/~campbell/cs65/lecture08/lecture08.html

--- app/src/main/java/com/example/studio08/verysimplepodcast/MainActivity.java
+++ app/src/main/java/com/example/studio08/verysimplepodcast/MainActivity.java
@@ -1,19 +1,41 @@
 package com.example.studio08.verysimplepodcast;
 
-import android.content.Intent;
+import android.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.studio08.verysimplepodcast.view.SlidingTabLayout;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    SlidingTabLayout slidingTabLayout;
+    ActionTabsViewPagerAdapter myViewPagerAdapter;
+    ViewPager viewPager;
+    ArrayList fragments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, FeedSelectorActivity.class);
-        startActivity(intent);
+
+        // sliding tab
+
+        slidingTabLayout = (SlidingTabLayout) findViewById(R.id.tab);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        fragments = new ArrayList<Fragment>();
+
+        myViewPagerAdapter =new ActionTabsViewPagerAdapter(getFragmentManager(), fragments);
+
+        // end sliding tab
+
+//        Intent intent = new Intent(this, FeedSelectorActivity.class);
+//        startActivity(intent);
     }
 
 
