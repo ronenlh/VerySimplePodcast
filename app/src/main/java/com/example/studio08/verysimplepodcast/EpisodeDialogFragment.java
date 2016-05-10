74180d11aa9f1ca015f7c979af8aa24b01a57abc
ronenlh <ronenmail@gmail.com>
2016-05-10 18:11:33 +0300

    Changed AlertDialog to a custom Layout.

--- app/src/main/java/com/example/studio08/verysimplepodcast/EpisodeDialogFragment.java
+++ app/src/main/java/com/example/studio08/verysimplepodcast/EpisodeDialogFragment.java
@@ -5,6 +5,9 @@ import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by studio08 on 5/10/2016.
@@ -29,11 +32,19 @@ public class EpisodeDialogFragment extends DialogFragment {
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
-        builder.setTitle(""+title)
-                .setMessage(""+description)
-//                .setMessage(""+author)
-//                .setMessage(""+pubDate)
-                .setPositiveButton(R.string.dialog_play, new DialogInterface.OnClickListener() {
+        // Get the layout inflater
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        View content = inflater.inflate(R.layout.fragment_dialog_episode, null);
+        builder.setView(content);
+        ((TextView) content.findViewById(R.id.dialog_title)).setText(""+title);
+        ((TextView) content.findViewById(R.id.dialog_author)).setText(""+author);
+        ((TextView) content.findViewById(R.id.dialog_date)).setText(""+pubDate);
+        ((TextView) content.findViewById(R.id.dialog_description)).setText(""+description);
+
+        builder.setPositiveButton(R.string.dialog_play, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
 
                     }
