299e49e288e8a1690e4bd19e96b7699b29031489
Ronen Lahat <ronenmail@gmail.com>
2016-05-15 11:55:24 +0300

    begun implementing mediaPlayer and it prepares and plays asynchronously. Need to add mre states like release(), need to change button states and implement it.

--- app/src/main/java/com/example/studio08/verysimplepodcast/MiniPlayerFragment.java
+++ app/src/main/java/com/example/studio08/verysimplepodcast/MiniPlayerFragment.java
@@ -1,6 +1,7 @@
 package com.example.studio08.verysimplepodcast;
 
 import android.content.Intent;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
@@ -15,6 +16,8 @@ import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.Toast;
 
+import java.io.IOException;
+
 /**
  * Created by Ronen on 16/4/16.
  */
@@ -23,7 +26,7 @@ public class MiniPlayerFragment extends Fragment {
     View miniplayer = null;
 
     private ImageView playButton;
-    private boolean isPlayButton = false;
+    private boolean isPlayButton = true;
     private MediaPlayer mediaPlayer = null;
     private int fileDuration;
     private SeekBar progressBar = null;
@@ -33,22 +36,43 @@ public class MiniPlayerFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        startViews();
+
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         miniplayer = inflater.inflate(R.layout.miniplayer_fragment, container, false);
+        startViews();
         return miniplayer;
     }
 
     public void startPlayer(String mediaUrl) {
         Log.d("miniplayer", mediaUrl);
+        MediaPlayer mediaPlayer = null;
+        if (mediaPlayer == null) {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            try {
+                mediaPlayer.setDataSource(mediaUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            mediaPlayer.prepareAsync();
+
+            // finalMediaPlayer autocompleted to comply with inner class requirements
+            final MediaPlayer finalMediaPlayer = mediaPlayer;
+            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    finalMediaPlayer.start();
+//                    changePlayButtonState();
+                }
+            });
+        }
     }
 
     public void changePlayButtonState(){
-        isPlayButton = !isPlayButton;
         if(isPlayButton) {
             if (mediaPlayer != null) {
                 playButton.setImageResource(R.drawable.ic_pause_24dp);
@@ -60,6 +84,7 @@ public class MiniPlayerFragment extends Fragment {
                 mediaPlayer.pause();
             }
         }
+        isPlayButton = !isPlayButton;
     }
 
     private void startViews() {
