f5303ddc33da5b017187494435cec8021ba4ab21
ronenlh <ronenmail@gmail.com>
2016-05-17 10:24:12 +0300

    Added Picasso dependency and started to implement it on bindView and newView of FeedCursorAdapter

--- app/src/main/java/com/example/studio08/verysimplepodcast/FeedCursorAdapter.java
+++ app/src/main/java/com/example/studio08/verysimplepodcast/FeedCursorAdapter.java
@@ -3,10 +3,14 @@ package com.example.studio08.verysimplepodcast;
 import android.content.Context;
 import android.database.Cursor;
 import android.support.v4.widget.CursorAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.SimpleCursorAdapter;
 
 import com.example.studio08.verysimplepodcast.database.FeedReaderContract;
 import com.example.studio08.verysimplepodcast.database.FeedsContract;
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by studio08 on 5/4/2016.
@@ -25,5 +29,17 @@ public class FeedCursorAdapter extends SimpleCursorAdapter {
         int layout = R.layout.main_row;
         int flags = CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER;
         return new FeedCursorAdapter(context, layout, c, from, to, flags);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        super.bindView(view, context, cursor);
+        ImageView imageView = (ImageView) view.findViewById(R.id.thumbnail_imageView);
+        Picasso.with(context).load("http://i.imgur.com/DvpvklR.png").into(imageView);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return super.newView(context, cursor, parent);
     }
 }
