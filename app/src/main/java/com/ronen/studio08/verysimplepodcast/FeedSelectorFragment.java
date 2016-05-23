4af8adda3cbd84dcb8de8d5d939518cc2fb5c464
ronenlh <ronenmail@gmail.com>
2016-05-23 13:24:21 +0300

    begun to implement contextual action bar for feed selector

--- app/src/main/java/com/ronen/studio08/verysimplepodcast/FeedSelectorFragment.java
+++ app/src/main/java/com/ronen/studio08/verysimplepodcast/FeedSelectorFragment.java
@@ -10,8 +10,14 @@ import android.support.annotation.Nullable;
 
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.ListFragment;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.ActionMode;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -29,13 +35,7 @@ public class FeedSelectorFragment extends ListFragment implements AdapterView.On
 //    ArrayList<Feed> podcastFeedList;
     onFeedSelectedListener mCallback;
 
-    @Override
-    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-        Cursor cursor = (Cursor) parent.getItemAtPosition(position);
-        DialogFragment dialogFragment = FeedDialogFragment.newInstance(cursor.getString(1));
-        dialogFragment.show(getFragmentManager(), "dialog");
-        return false;
-    }
+    ActionMode mActionMode;
 
     interface onFeedSelectedListener {
         void onFeedSelected(int position, String feedUrl);
@@ -108,14 +108,65 @@ public class FeedSelectorFragment extends ListFragment implements AdapterView.On
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
-
         Cursor cursor = (Cursor) l.getItemAtPosition(position);
         Log.d("onListItemClick", cursor.getString(3));
         mCallback.onFeedSelected(position, cursor.getString(3));
-        super.onListItemClick(l, v, position, id);
     }
 
     public void deleteRow() {
 
+    }
+
+    private ActionMode.Callback mActionModeCallback = new ActionMode.Callback() {
+
+        // Called when the action mode is created; startActionMode() was called
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            // Inflate a menu resource providing context menu items
+            MenuInflater inflater = mode.getMenuInflater();
+            inflater.inflate(R.menu.feed_menu, menu);
+            return true;
+        }
+
+        // Called each time the action mode is shown. Always called after onCreateActionMode, but
+        // may be called multiple times if the mode is invalidated.
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false; // Return false if nothing is done
+        }
+
+        // Called when the user selects a contextual menu item
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.delete:
+                    deleteRow();
+                    mode.finish(); // Action picked, so close the CAB
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        // Called when the user exits the action mode
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            mActionMode = null;
+        }
+    };
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+//        Cursor cursor = (Cursor) parent.getItemAtPosition(position);
+//        DialogFragment dialogFragment = FeedDialogFragment.newInstance(cursor.getString(1));
+//        dialogFragment.show(getFragmentManager(), "dialog");
+
+        if (mActionMode != null) {
+            return false;
+        }
+
+        mActionMode = ((AppCompatActivity) getActivity()).startSupportActionMode(mActionModeCallback);
+        view.setSelected(true);
+        return true;
     }
 }
