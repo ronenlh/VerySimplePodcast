710b8f5dbb9539096ee2751f65ab48b3ec86ea5f
Ronen Lahat <ronenmail@gmail.com>
2016-05-27 16:11:30 +0300

    Begun to implement Firebase Cloud Messaging.

---
+++ app/src/main/java/com/ronen/studio08/verysimplepodcast/firebase/MyFirebaseMessagingService.java
@@ -0,0 +1,13 @@
+package com.ronen.studio08.verysimplepodcast.firebase;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * Created by Ronen on 27/5/16.
+ */
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    // A service that extends FirebaseMessagingService.
+    // This is required if you want to do any message handling beyond receiving notifications on apps in the background.
+    // To receive notifications in foregrounded apps, to receive data payload, to send upstream messages, and so on, you must extend this service.
+
+}
