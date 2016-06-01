2cc04b57af6fc0742c8ffbf1280230b8672e0b40
ronenlh <ronenmail@gmail.com>
2016-06-01 16:22:25 +0300

    Great achievement unlocked. Changes to Sample feed's base class to receive an array instead of an object containing an array. Now the sample feeds are from a cloud querrying a mongo database.

---
+++ app/src/main/java/com/ronen/studio08/verysimplepodcast/retrofitCloud/SampleFeed.java
@@ -0,0 +1,33 @@
+package com.ronen.studio08.verysimplepodcast.retrofitCloud;
+
+/**
+ * Created by studio08 on 6/1/2016.
+ */
+public class SampleFeed {
+
+    private String title;
+    private String description;
+    private String feedUrl;
+    private String imgHref;
+
+    @Override
+    public String toString() {
+        return title + ", " + description + ", " + feedUrl + ", " + imgHref + "\n";
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getFeedUrl() {
+        return feedUrl;
+    }
+
+    public String getImgHref() {
+        return imgHref;
+    }
+}
