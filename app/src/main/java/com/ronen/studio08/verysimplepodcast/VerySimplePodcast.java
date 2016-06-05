ccc81b974b477e58758ced5c5eb5373a711847d3
Ronen Lahat <ronenmail@gmail.com>
2016-06-05 14:30:53 +0300

    begun personalizing country code, but running into crashes.

--- app/src/main/java/com/ronen/studio08/verysimplepodcast/VerySimplePodcast.java
+++ app/src/main/java/com/ronen/studio08/verysimplepodcast/VerySimplePodcast.java
@@ -1,6 +1,10 @@
 package com.ronen.studio08.verysimplepodcast;
 
 import android.app.Application;
+import android.content.res.Configuration;
+import android.util.Log;
+
+import java.util.Locale;
 
 /**
  * Created by studio08 on 5/19/2016.
@@ -15,9 +19,27 @@ import android.app.Application;
  * include Context.getApplicationContext() as a Context argument when invoking your singleton's getInstance() method.
  */
 public class VerySimplePodcast extends Application {
+
+    /*
+    * Using an Application (don't forget to define it in your manifest) we get the default locale when the app starts (onCreate())
+    * and we update it when the user changes the language in the Android settings (onConfigurationChanged(Configuration)).
+    * That's all there is. Whenever you need to know what the system default language was before you used your setDefault call,
+    * sDefSystemLanguage will give you the answer.
+    * */
+    public static String sDefSystemLanguage;
+
     @Override
     public void onCreate() {
         super.onCreate();
-        // Created this class extending Application and added the android:name in the Manifest so I can add LeakCanary.
+        sDefSystemLanguage = Locale.getDefault().getLanguage();
+        Log.d("sDefSystemLanguage", sDefSystemLanguage);
     }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        sDefSystemLanguage = newConfig.locale.getLanguage();
+    }
+
 }
