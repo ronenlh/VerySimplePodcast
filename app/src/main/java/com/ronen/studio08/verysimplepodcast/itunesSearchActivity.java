9af2827ff11586e64e10640d00838250e0d2d6c5
Ronen Lahat <ronenmail@gmail.com>
2016-06-05 11:41:54 +0300

    Begun implementing itunes search api

---
+++ app/src/main/java/com/ronen/studio08/verysimplepodcast/itunesSearchActivity.java
@@ -0,0 +1,13 @@
+package com.ronen.studio08.verysimplepodcast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class itunesSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_itunes_search);
+    }
+}
